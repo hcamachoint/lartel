418168d3d763cc52adc4fe0bf8a7bb2b0fd9f7c3
Hugo Camacho <hcamacho.int@gmail.com>
2018-01-08 14:38:34 -0400

    Hola

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
     $botId = $response->getId();
     $firstName = $response->getFirstName();
     $username = $response->getUsername();
-    return view('welcome', ['response' => $response, 'botName' => $firstName, 'username' => $username]);
+    return view('welcome', ['response' => $response, 'username' => $username, 'botId' => $botId]);
 });
 
 Route::get('/sethook', function () {
